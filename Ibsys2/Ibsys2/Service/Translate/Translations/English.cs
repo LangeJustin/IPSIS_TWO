999e9c3c0b9b867af83ddb047cd6f5fb448e8f1f
Justin Lange <justin@langejustin.com>
2019-05-17 10:54:00 +0200

    resolving stuff
    Signed-off-by: Justin Lange <justin@langejustin.com>

--- Ibsys2/Ibsys2/Service/Translate/Translations/English.cs
+++ Ibsys2/Ibsys2/Service/Translate/Translations/English.cs
@@ -15,6 +15,9 @@ namespace Ibsys2.Service {
 
         private static void Translate() {
             _translate.Add("HELLO", "Hello");
+            _translate.Add("REFRESHCHILD", "refresh");
+            _translate.Add("REFRESHWOMEN", "refresh");
+            _translate.Add("REFRESHMAN", "refresh");
             _translate.Add("SETTINGS_SAVED", "Settings saved! Would you like to restart the Program? Without a restart translate errors will occur!");
             _translate.Add("SETTINGS", "Settings");
             _translate.Add("XML_ERROR", "This is not a XML File!");
